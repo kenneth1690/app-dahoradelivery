ebbd3d600c47129dbb3c5dcf626b72e46d5b975c
Manogel <manoelgomes53@gmail.com>
2020-09-13 15:43:20 -0300

    configuration ESLint, Prettier, typescript  and structure

--- babel.config.js
+++ babel.config.js
@@ -1,3 +1,26 @@
 module.exports = {
   presets: ['module:metro-react-native-babel-preset'],
+  env: {
+    production: {
+      plugins: ['transform-remove-console'],
+    },
+  },
+  plugins: [
+    [
+      'module-resolver',
+      {
+        cwd: 'babelrc',
+        extensions: ['.ts', '.tsx', '.js', '.ios.js', '.android.js'],
+        alias: {
+          '@contexts': './src/contexts',
+          '@pages': './src/pages',
+          '@components': './src/components',
+          '@services': './src/services',
+          '@assets': './src/assets',
+          '@libs': './src/libs',
+        },
+      },
+    ],
+    'jest-hoist',
+  ],
 };
