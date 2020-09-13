075817f91c8a25b2fbee2a00e100a62c0535c041
Manogel <manoelgomes53@gmail.com>
2020-09-13 17:04:22 -0300

    config: add ThemeProvider to config styled-components

--- src/App.tsx
+++ src/App.tsx
@@ -1,38 +1,44 @@
 import React from 'react';
 import { Text, SafeAreaView } from 'react-native';
 
+import { ThemeProvider } from 'styled-components';
+
+import lightTheme from './styles/themes/light';
+
 const App: React.FC = () => {
   return (
-    <SafeAreaView>
-      <Text
-        style={{
-          fontFamily: 'Nunito-Regular',
-        }}
-      >
-        App
-      </Text>
-      <Text
-        style={{
-          fontFamily: 'Nunito-SemiBold',
-        }}
-      >
-        App
-      </Text>
-      <Text
-        style={{
-          fontFamily: 'Nunito-Bold',
-        }}
-      >
-        App
-      </Text>
-      <Text
-        style={{
-          fontFamily: 'Nunito-ExtraBold',
-        }}
-      >
-        App
-      </Text>
-    </SafeAreaView>
+    <ThemeProvider theme={lightTheme}>
+      <SafeAreaView>
+        <Text
+          style={{
+            fontFamily: 'Nunito-Regular',
+          }}
+        >
+          App
+        </Text>
+        <Text
+          style={{
+            fontFamily: 'Nunito-SemiBold',
+          }}
+        >
+          App
+        </Text>
+        <Text
+          style={{
+            fontFamily: 'Nunito-Bold',
+          }}
+        >
+          App
+        </Text>
+        <Text
+          style={{
+            fontFamily: 'Nunito-ExtraBold',
+          }}
+        >
+          App
+        </Text>
+      </SafeAreaView>
+    </ThemeProvider>
   );
 };
 
